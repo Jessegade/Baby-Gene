ac205f9cf76eaeb6c3338742f16771d70bc7eb5a
tharamalai <s7070054@kmitl.ac.th>
2014-11-21 14:47:41 +0700

    first open project

---
+++ project1.py
@@ -0,0 +1,26 @@
+'''
+Baby Gene
+PSIT Project
+By Tharathip Malaimarn(tharamalai=>nam)
+    Pawarisa Thong-ngoen(Jessegade=>gade)
+APPLICATION to tell you
+- chance of gene's your baby
+--Blood Group
+--Genetic Diseases
+------Autosome Recessive
+--------Thalassemia
+--------Cystic fibrosis
+--------Phenylketonuria(PKU)
+--------Albinism
+------Autosome Dominant
+--------Achondroplasia
+------Sex-linked Gene
+--------X-linked gene
+----------Color blindness
+----------Glocose-6-phosphate dehydrogenase(G-6-PD)
+----------Hemophilia
+----------Duchennne's muscular dystrophy
+----------Hypohidrotic Ectodermal Dysplasia
+-----Sickle cell anemia
+************this is only chance************
+'''
