7447889af723d4deced5a5290a1d0c0517d71118
Jessegade <s7070068@kmitl.ac.th>
2014-12-02 14:05:14 +0700

    .

--- project1.py
+++ project1.py
@@ -31,28 +31,47 @@ import tkMessageBox
 root = Tk()
 root.title('Baby Gene')                  
 
-def bloodgroup():
+def bloodtype():
     win1 = Toplevel(root)
-    win1.title('Blood Group')
+    win1.title('Blood Type')
     #win1.configure(background = "white")
     global mom, dad
+    
+    mom_text = Label(win1, text='Mother Blood Type: ').grid(row=0, column=0)
+
     mom = StringVar(win1)
     mom.set('A')
-    mom_grp = OptionMenu(win1, mom, 'A', 'B', 'AB', 'O').pack()
+    mom_type = OptionMenu(win1, mom, 'A', 'B', 'AB', 'O').grid(row=0, column=1)
+
+    dad_text = Label(win1, text='Father Blood Type: ').grid(row=2, column=0)
+    
     dad = StringVar(win1)
     dad.set('A')
-    dad_grp = OptionMenu(win1, dad, 'A', 'B', 'AB', 'O').pack()
+    dad_type = OptionMenu(win1, dad, 'A', 'B', 'AB', 'O').grid(row=2, column=1)
+    ok_button = Button(win1, text='ok',command=ok).grid(row=3, column=1)
+def ok():
+    mom_and_dad = str(mom.get())+str(dad.get())
+    group = {'AA': ['A 50%', 'O 50%'], \
+             'BB': ['B 50%', 'O 50%'], \
+             'ABAB': ['A 33.33%', 'B 33.33%' ,'AB 33.33%'], \
+             'OO': ['O 100%'], \
+             'AB': ['A 25%', 'B 25%', 'AB 25%', 'O 25%'], \
+             'BA': ['A 25%', 'B 25%', 'AB 25%', 'O 25%'], \
+             'AAB': ['A 33.33%', 'B 33.33%' ,'AB 33.33%'], \
+             'ABA': ['A 33.33%', 'B 33.33%' ,'AB 33.33%'], \
+             'BAB': ['A 33.33%', 'B 33.33%' ,'AB 33.33%'], \
+             'ABB': ['A 33.33%', 'B 33.33%' ,'AB 33.33%'], \
+             'ABO': ['A 50%', 'B 50%'], \
+             'OAB': ['A 50%', 'B 50%'], \
+             'AO': ['A 50%', 'O 50%'], \
+             'OA': ['A 50%', 'O 50%'], \
+             'BO': ['B 50%', 'O 50%'], \
+             'OB': ['B 50%', 'O 50%']}
+    tkMessageBox.showinfo('Result',' or '.join(group[mom_and_dad]))
     
-    ok_button = Button(win1, text='ok',command=ok).pack()
-def ok(x):
-    '''
-    if x == 'A':
-        tkMessageBox.showinfo('asdsaf','erwrt')
-    '''
-    print mom.get()
 
 
-bldgrp = Button(root, text = 'Blood Group' , command = bloodgroup).pack()
+bldgrp = Button(root, text = 'Blood Group' , command = bloodtype).pack()
 
 root.mainloop()
 
